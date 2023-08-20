ef8b052c4e4aed27c241dddf7632db6cfd6cd58a
minxing. bao <minxing. bao@wimisoft.com>
2023-08-20 14:21:07 +0800

    feat : ASP.NET Web Form项目新增自定义HttpHandler示例代码

---
+++ 001 - ASP.NET Web Form/SampleCode001/Pipeline/CustomHttpHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SampleCode001
+{
+    /// <summary>
+    /// 自定义HttpHandler
+    /// 
+    /// 我们可以从请求级出发，避开默认机制，动态响应 .log（自定义）后缀的请求
+    /// </summary>
+    public class CustomHttpHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        /// <summary>
+        /// 处理请求
+        /// </summary>
+        /// <param name="context"></param>
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Write("这个从自定义Handler处理输出");
+        }
+    }
+}
