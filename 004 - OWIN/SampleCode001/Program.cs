40507da53b727aceee59fd9adb960cea27ad03dc
minxing. bao <minxing. bao@wimisoft.com>
2023-08-20 14:31:25 +0800

    feat : 新增OWN框架基础示例

---
+++ 004 - OWIN/SampleCode001/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Hosting;
+
+namespace SampleCode001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                HttpClient client = new HttpClient();
+                Console.WriteLine(baseAddress);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 004 - OWIN/SampleCode001/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace SampleCode001
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // 创建 Web API 的配置
+            var config = new HttpConfiguration();
+            // 启用标记路由
+            config.MapHttpAttributeRoutes();
+            // 默认的 Web API 路由
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // 将路由配置附加到 appBuilder
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
